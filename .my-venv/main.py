e84c9d9ff769fe24a903572092bb3dff5db04f13
jyoti <jyoti.sharma513@gmail.com>
2024-02-02 15:10:29 +0100

    first commit

---
+++ .my-venv/main.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+start_time = perf_counter()
+from scr.scraper import WikipediaScraper
+
+if __name__ == "__main__":
+    
+    
+    wiki = WikipediaScraper(base_url="https://country-leaders.onrender.com")
+
+    countries = wiki.get_countries()
+
+    if countries:
+        for country in countries:
+            wiki.get_leaders(country)
+
+        # Save the leaders data into a JSON file
+        wiki.to_json_file(filepath="leaders_data.json")
+print(f"\nTime spent to finish the task: {perf_counter() - start_time} seconds.")    
